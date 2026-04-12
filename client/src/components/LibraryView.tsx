a876b48337623b1792a7b30da611435e66749e55
Reachushere <reachushere@users.noreply.github.com>
2026-04-12 07:25:26 -0400

    Library: top+22, mid-12, bot-46, cppa+32, cfnf+50, casl+19

--- client/src/components/LibraryView.tsx
+++ client/src/components/LibraryView.tsx
@@ -3486,7 +3486,7 @@ export default function LibraryView({ isOpen, onClose, semesters: semestersProp,
             const collapseKey = `${semKey}::${course.code}`;
             const isCollapsed = collapsedCourses.has(collapseKey);
             return (
-            <div key={course.code} style={{ marginBottom: courseIdx < courseBooks.length - 1 ? (isCollapsed ? '20px' : '55px') : '0', position: 'relative', top: courseIdx === 0 ? '17px' : courseIdx === 1 ? '50px' : courseIdx === 2 ? '43px' : `${46 + courseIdx * 5}px` }}>
+            <div key={course.code} style={{ marginBottom: courseIdx < courseBooks.length - 1 ? (isCollapsed ? '20px' : '55px') : '0', position: 'relative', top: courseIdx === 0 ? '39px' : courseIdx === 1 ? '38px' : courseIdx === 2 ? '-3px' : `${0 + courseIdx * 5}px` }}>
               <div style={{
                 display: 'flex',
                 alignItems: 'center',
@@ -3497,7 +3497,7 @@ export default function LibraryView({ isOpen, onClose, semesters: semestersProp,
                 cursor: 'pointer',
                 userSelect: 'none',
                 position: 'relative',
-                top: courseIdx === 0 ? '168px' : courseIdx === 1 ? '169px' : courseIdx === 2 ? '171px' : '155px',
+                top: courseIdx === 0 ? '200px' : courseIdx === 1 ? '219px' : courseIdx === 2 ? '190px' : '155px',
                 zIndex: 10,
                 marginLeft: 'calc(30% + 190px)',
               }}
