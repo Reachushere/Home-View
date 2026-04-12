6b7b1cd55a80e5b41ae75fd6c1dbe05786ba5262
Reachushere <reachushere@users.noreply.github.com>
2026-04-12 06:40:03 -0400

    Library: align all syllabus buttons and titles to same left position

--- client/src/components/LibraryView.tsx
+++ client/src/components/LibraryView.tsx
@@ -3496,8 +3496,7 @@ export default function LibraryView({ isOpen, onClose, semesters: semestersProp,
                 position: 'relative',
                 top: courseIdx === 0 ? '163px' : courseIdx === 1 ? '163px' : courseIdx === 2 ? '164px' : '155px',
                 zIndex: 10,
-                width: 'fit-content',
-                margin: '0 auto',
+                marginLeft: '30%',
               }}
               onClick={() => {
                 setCollapsedCourses(prev => {
