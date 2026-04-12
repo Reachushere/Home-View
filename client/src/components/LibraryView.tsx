e9197ebda7e3e9fbab6091831f05f554f5f9d5ee
Reachushere <reachushere@users.noreply.github.com>
2026-04-12 07:54:15 -0400

    Library: cppa title up 2px

--- client/src/components/LibraryView.tsx
+++ client/src/components/LibraryView.tsx
@@ -3497,7 +3497,7 @@ export default function LibraryView({ isOpen, onClose, semesters: semestersProp,
                 cursor: 'pointer',
                 userSelect: 'none',
                 position: 'relative',
-                top: courseIdx === 0 ? '203px' : courseIdx === 1 ? '320px' : courseIdx === 2 ? '320px' : '155px',
+                top: courseIdx === 0 ? '201px' : courseIdx === 1 ? '320px' : courseIdx === 2 ? '320px' : '155px',
                 zIndex: 10,
                 marginLeft: 'calc(30% + 215px)',
               }}
