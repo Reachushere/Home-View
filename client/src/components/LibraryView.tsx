4a2b5edb301e8ef7a2293c238d9eb0b403701ff7
Reachushere <reachushere@users.noreply.github.com>
2026-04-12 18:56:42 -0400

    Move labels vertically to match book row shifts

--- client/src/components/LibraryView.tsx
+++ client/src/components/LibraryView.tsx
@@ -3652,7 +3652,7 @@ export default function LibraryView({ isOpen, onClose, semesters: semestersProp,
                 cursor: 'pointer',
                 userSelect: 'none',
                 position: 'relative',
-                top: courseIdx === 0 ? '225px' : courseIdx === 1 ? '340px' : courseIdx === 2 ? (minimizedReaders.size > 0 ? '300px' : '335px') : '165px',
+                top: courseIdx === 0 ? '224px' : courseIdx === 1 ? '339px' : courseIdx === 2 ? (minimizedReaders.size > 0 ? '301px' : '336px') : '164px',
                 zIndex: 10,
                 marginLeft: 'calc(30% + 229px)',
               }}
