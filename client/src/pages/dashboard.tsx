67ad4fae0b468e889fe9e203d324553b43cfd6b6
Reachushere <reachushere@users.noreply.github.com>
2026-04-15 18:43:49 -0400

    Fix scholarship wizard: remove auto-advance on date picker change so browsing months doesn't skip to next step

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -23372,11 +23372,6 @@ export default function Dashboard() {
                     onChange={(e) => {
                       const val = e.target.value;
                       setScholarshipForm(p => ({ ...p, [wizardSteps[scholarshipWizardStep].field]: val }));
-                      if (val && /^\d{4}-\d{2}-\d{2}$/.test(val)) {
-                        setTimeout(() => {
-                          if (scholarshipWizardStep < wizardSteps.length - 1) setScholarshipWizardStep(s => s + 1);
-                        }, 300);
-                      }
                     }}
                     className="w-full px-3 py-2 rounded border border-white/20 bg-white/5 text-white text-[11px] focus:outline-none focus:border-white/40 [color-scheme:dark]"
                     data-testid={`input-scholarship-${wizardSteps[scholarshipWizardStep].field}`}
