405d44bbac4dcbe782308fd9ef99425e541c6fc4
Reachushere <reachushere@users.noreply.github.com>
2026-04-19 23:12:20 -0400

    Add drag-drop-touch polyfill so HTML5 drag-and-drop works on touchscreens

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "drag-drop-touch";
 
 const originalFetch = window.fetch.bind(window);
 window.fetch = (input: RequestInfo | URL, init?: RequestInit) => {
